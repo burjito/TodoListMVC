f7d02334c68c41d8235774a08c3c3b4376cf335f
Jedd Aguilar <jeddeishenaguilar@gmail.com>
2025-05-16 22:27:48 +0800

    Update TodoItem model

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,10 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TodoListMVC.Models
 {
-    public enum Category { Home, School, Personal }
     public enum Priority { Low, Medium, High }
-    public enum Status { Pending, Done }
+    public enum Status { Pending, Done, Deleted }
 
     public class TodoItem
     {
@@ -15,12 +15,13 @@ namespace TodoListMVC.Models
 
         public required string Description { get; set; }
 
-        public Category Category { get; set; }
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
 
         public Priority Priority { get; set; }
 
         public Status Status { get; set; } = Status.Pending;
-
-        public bool IsHidden { get; set; } = false;
     }
 }
--- obj/Debug/net9.0/TodoListMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoListMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f5caebb069c9ac69c3cfdade0f5bf0432d26293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39702904f7475ab940cc840382bf4ae812216a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
