383f9d20046e95fb74a4597f3331c3ef095dbb35
Jedd Aguilar <jeddeishenaguilar@gmail.com>
2025-05-16 21:35:55 +0800

    Fix Razor option tag attribute error in Index.cshtml

--- obj/Debug/net9.0/TodoListMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoListMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfce91c53a3862c902afdd4e3022de67d1bf556a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
