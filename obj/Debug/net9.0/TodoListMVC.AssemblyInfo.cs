57239813b5bdb021e99802f9bc1a178cfa8664b8
Jedd Aguilar <jeddeishenaguilar@gmail.com>
2025-05-17 12:00:28 +0800

    Edit footer

--- obj/Debug/net9.0/TodoListMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoListMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3d5e225fa30967024d1db61e79099c15dfb9ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e74bca72279ec5368abad4c1d824344a72fdeb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
